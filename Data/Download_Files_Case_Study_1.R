5886a73e92577e0e97c779b71bd065c0ede41008
Phillip Efthimion <phillip.efthimion@gmail.com>
2016-10-31 18:56:07 -0400

    Add files via upload
    Uploads the data files needed for analysis

---
+++ Data/Download_Files_Case_Study_1.R
@@ -0,0 +1,28 @@
+# Phillip Efthimion
+#Intro to Data Science
+#Case Study 1
+
+# Required packages
+
+library(dplyr)
+library(ggplot2)
+
+# Downloading the data files
+
+
+
+# First we have to set our working directory so we know where to call and files we use from 
+getwd()
+
+# When coding on my computer
+# setwd("/Users/Phillip/Documents/SMU/Intro to Data Science/Case Study 1")
+
+# On Github
+setwd("https://github.com/pefthimion/Case-Study-1")
+
+
+# Download the files from the internet and save them into our workspace
+
+GDP <- read.csv("getdata%2Fdata%2FGDP.csv", sep = ",", header = TRUE)
+Edu <- read.csv("getdata%2Fdata%2FEDSTATS_Country.csv", sep = ",", header = TRUE)
+
