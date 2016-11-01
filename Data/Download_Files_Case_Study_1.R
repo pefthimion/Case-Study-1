da8d98746a06939e82240cc5d2b8c03b0de36dae
Phillip Efthimion <phillip.efthimion@gmail.com>
2016-11-01 19:10:19 -0400

    reload

--- Data/Download_Files_Case_Study_1.R
+++ Data/Download_Files_Case_Study_1.R
@@ -12,18 +12,17 @@ library(ggplot2)
 
 
 # First we have to set our working directory so we know where to call and files we use from 
-# getwd()
-# cat("File is read")
+getwd()
+
 # When coding on my computer
 # setwd("/Users/Phillip/Documents/SMU/Intro to Data Science/Case Study 1")
 
 # On Github
-# setwd("https://github.com/pefthimion/Case-Study-1/tree/master/Data")
-# cat("Directory is set")
+# setwd("https://github.com/pefthimion/Case-Study-1")
+
 
 # Download the files from the internet and save them into our workspace
 
 GDP <- read.csv("getdata%2Fdata%2FGDP.csv", sep = ",", header = TRUE)
 Edu <- read.csv("getdata%2Fdata%2FEDSTATS_Country.csv", sep = ",", header = TRUE)
 
-cat("files read")
